014dc3714ee8234a5420eb0ae1feff2c5408626c
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-03-30 15:21:49 +0200

    add i in div literal string

--- script.js
+++ script.js
@@ -14,45 +14,45 @@ for(let i = 1; i < 101; i++){
     console.log('FIZZ');
     document.getElementById("my-container").innerHTML += `
                                             <div  class="box-fizz">
-                                              <p>FIZZ</p>
+                                              <p>${i} FIZZ</p>
                                             </div>`
     
    }
    //se il resto della divisione tra i e 3 ed i e 5  e' strettamente uguale a 0 
    //(quindi vuol dire che il numero contenuto nell'i  e' multiplo di 15)
-   if(i % 3 === 0 && i % 5 === 0 ){
+    if(i % 3 === 0 && i % 5 === 0){
        
        
        console.log('FIZZBUZZ');
        
        document.getElementById("my-container").innerHTML += `
        <div  class="box-fizzbuzz">
-       <p>FIZZBUZZ</p>
+       <p>${i} FIZZBUZZ</p>
        </div>`;
        //se il resto della divisione tra i e 5 e' strettamente uguale a 0 
        //(quindi vuol dire che il numero contenuto nell'i  e' multiplo di 5)
     }
-   else if(i % 5 === 0){
+    else if(i % 5 === 0){
      
        
        console.log('BUZZ');
        document.getElementById("my-container").innerHTML += `
                                                <div  class="box-buzz">
-                                                 <p>BUZZ</p>
+                                                 <p>${i} BUZZ</p>
                                                </div>`;
        
    }
 
 
-   else{
-    
-    //altrimenti stampami tutti gli altri
-    console.log(i);
-    document.getElementById("my-container").innerHTML += `
-                                            <div  class="box-index">
-                                              <p>${i}</p>
-                                            </div>`
-   };
+     else{
+            
+            //altrimenti stampami tutti gli altri
+         console.log(i);
+         document.getElementById("my-container").innerHTML += `
+                                                    <div  class="box-index">
+                                                    <p>${i}</p>
+                                                    </div>`
+     };  
 
 
 
