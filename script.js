aa064a0fa9cba48ce3ade94bb633864eb1d1e0fb
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-03-30 15:40:33 +0200

    add shadow to box in css

--- script.js
+++ script.js
@@ -5,34 +5,34 @@ let boxBuzz =''
 //creo un ciclo for per iterare nnumeri da 1 a 100
 for(let i = 1; i < 101; i++){
 
-   //se il resto della divisione tra i e 3 e' strettamente uguale a 0 
-   //(quindi vuol dire che il numero contenuto nell'i  e' multiplo di 3)
+    //se il resto della divisione tra i e 3 ed i e 5  e' strettamente uguale a 0 
+    //(quindi vuol dire che il numero contenuto nell'i  e' multiplo di 15)
+    if(i % 3 === 0 && i % 5 === 0){
+        
+        
+        console.log('FIZZBUZZ');
+        
+        document.getElementById("my-container").innerHTML += `
+        <div  class="box-fizzbuzz">
+        <p>${i} FIZZBUZZ</p>
+        </div>`;}
+    //se il resto della divisione tra i e 3 e' strettamente uguale a 0 
+    //(quindi vuol dire che il numero contenuto nell'i  e' multiplo di 3)
 
-   if(i % 3 === 0){
+     if(i % 3 === 0){
     
 
-    console.log('FIZZ');
-    document.getElementById("my-container").innerHTML += `
+     console.log('FIZZ');
+     document.getElementById("my-container").innerHTML += `
                                             <div  class="box-fizz">
                                               <p>${i} FIZZ</p>
                                             </div>`
     
-   }
-   //se il resto della divisione tra i e 3 ed i e 5  e' strettamente uguale a 0 
-   //(quindi vuol dire che il numero contenuto nell'i  e' multiplo di 15)
-    if(i % 3 === 0 && i % 5 === 0){
-       
-       
-       console.log('FIZZBUZZ');
-       
-       document.getElementById("my-container").innerHTML += `
-       <div  class="box-fizzbuzz">
-       <p>${i} FIZZBUZZ</p>
-       </div>`;
+   
        //se il resto della divisione tra i e 5 e' strettamente uguale a 0 
        //(quindi vuol dire che il numero contenuto nell'i  e' multiplo di 5)
-    }
-    else if(i % 5 === 0){
+     }
+     else if(i % 5 === 0){
      
        
        console.log('BUZZ');
@@ -41,7 +41,7 @@ for(let i = 1; i < 101; i++){
                                                  <p>${i} BUZZ</p>
                                                </div>`;
        
-   }
+    }
 
 
      else{
